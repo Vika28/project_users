b0972032b81f8686fef4edf22013d9d855a9007b
Viktoriia Nakoryk <Viktoriia_Nakoryk@epam.com>
2022-05-19 14:00:52 +0200

    finished 8 section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div>
+    const [usersList, setUsersList] = useState([]);
 
+    const addUserHandler = (uName, uAge) => {
+        console.log('users app', usersList, uName, uAge);
+        setUsersList((prevUsersList) => {
+            return [...prevUsersList,
+                { name: uName, age: uAge, id: Math.random().toString() }];
+        })
+    }
+    return (
+    <div>
+        <AddUser
+            onAddUser={addUserHandler}
+        />
+        <UsersList
+            users={usersList}
+        />
     </div>
-  );
+    );
 }
 
 export default App;
